b08aab66d7249e7199d9a777f5e584c5f89ec1b2
Fabricio <fabriciogodoy95@hotmail.com>
2022-10-20 17:23:22 -0300

    entrance products

--- src/components/home/featureProducts/officeList/mobile/featureOfficeMobileMap.js
+++ src/components/home/featureProducts/officeList/mobile/featureOfficeMobileMap.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export const FeatureOfficeMobile = ({ officeMobile }) => {
 
   return (
-      <div className=" containerFeature containerFeatureMobileOn ">
+      <div className=" containerFeature containerFeatureMobileOn slide-in-left">
         
 <div className="containerTitle">
           
