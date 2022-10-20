0e5f76074861388546a77e5ad776ba2cbfb00312
Fabricio <fabriciogodoy95@hotmail.com>
2022-10-20 17:32:10 -0300

    entrance products

--- src/components/home/logosRedirect.js
+++ src/components/home/logosRedirect.js
@@ -3,24 +3,18 @@ import React from 'react'
 export const LogosRedirect = () => {
   return (
     <div className="containerLogoRedirect">
-    <a href="#">
       <img
         src="https://github.com/FabricioGodoy/imagenes/blob/main/SN-LLC/Microsoft_logo_(2012).svg.png?raw=true"
         className="logoRedirect"
       />
-    </a>
-    <a href="#">
       <img
         src="https://github.com/FabricioGodoy/imagenes/blob/main/SN-LLC/5a3a2120266c24.67883703151375900815749407.png?raw=true"
         className="logoRedirect logoIntelHeight"
       />
-    </a>
-    <a href="#">
       <img
         src="https://github.com/FabricioGodoy/imagenes/blob/main/SN-LLC/AMD_Logo.svg.png?raw=true"
         className="logoRedirect"
       />
-    </a>
 
   </div>
   )
